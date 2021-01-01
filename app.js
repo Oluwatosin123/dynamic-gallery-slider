e6dd1903e63380f42377952c5b5cf7c53bd7811e
Ajose Tosin <tosinemidastouch@gmail.com>
2021-01-01 21:13:13 +0100

    final code reractoring

--- app.js
+++ app.js
@@ -1,60 +1,26 @@
 'use strict'
 
-const fulImg = document.querySelector('.full-img');
+const fullImg = document.querySelector('.full-img');
+const smallImg = document.querySelectorAll('.gallery img');
+const modal = document.querySelector('.modal');
 
+smallImg.forEach(img => {
+    img.addEventListener('click', () => {
+        modal.classList.add('open')
+        fullImg.classList.add('open')
 
+        //changing the images dynamically
+        const originalQuality = img.getAttribute('alt');
+        fullImg.src = `img/full/${originalQuality}.jpg`
+    })
+});
 
+//removing the modal
+modal.addEventListener('click', (e) => {
+    // console.log(e);
+    if (e.target.classList.contains('modal')) {
+        modal.classList.remove('open');
+        fullImg.classList.remove('open')
+    }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fullImg = document.querySelector('.full-img');
-// const smallImg = document.querySelectorAll('.gallery img');
-// const modal = document.querySelector('.modal');
-
-// // console.log(smallImg);
-
-// smallImg.forEach(img => {
-//     img.addEventListener('click', function () {
-//         modal.classList.add('open');
-//         fullImg.classList.add('open');
-
-//         //changing the images dynamically
-//         const originalQuality = img.getAttribute('alt');
-//         fullImg.src = `img/full/${originalQuality}.jpg`
-//     });
-// });
-
-// modal.addEventListener('click', function (e) {
-//     if (e.target.classList.contains("modal")) {
-//         modal.classList.remove('open');
-//         fullImg.classList.remove('open');
-//     }
-// })

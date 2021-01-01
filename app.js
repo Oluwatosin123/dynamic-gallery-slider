7a395f991ca767880637e39a3544d739b568ca1a
Ajose Tosin <tosinemidastouch@gmail.com>
2021-01-01 20:45:51 +0100

    modifying the gallery dynamically

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 'use strict'
 
-const fullImg = document.querySelector('.full-img');
-const smallImg = document.querySelectorAll('.gallery img');
-const modal = document.querySelector('.modal');
+const fulImg = document.querySelector('.full-img');
 
-// console.log(smallImg);
 
-smallImg.forEach(img => {
-    img.addEventListener('click', function () {
-        modal.classList.add('open');
-        fullImg.classList.add('open');
 
-        //changing the images dynamically
-        const originalQuality = img.getAttribute('alt');
-        fullImg.src = `img/full/${originalQuality}.jpg`
-    });
-});
 
-modal.addEventListener('click', function (e) {
-    if (e.target.classList.contains("modal")) {
-        modal.classList.remove('open');
-        fullImg.classList.remove('open');
-    }
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fullImg = document.querySelector('.full-img');
+// const smallImg = document.querySelectorAll('.gallery img');
+// const modal = document.querySelector('.modal');
+
+// // console.log(smallImg);
+
+// smallImg.forEach(img => {
+//     img.addEventListener('click', function () {
+//         modal.classList.add('open');
+//         fullImg.classList.add('open');
+
+//         //changing the images dynamically
+//         const originalQuality = img.getAttribute('alt');
+//         fullImg.src = `img/full/${originalQuality}.jpg`
+//     });
+// });
+
+// modal.addEventListener('click', function (e) {
+//     if (e.target.classList.contains("modal")) {
+//         modal.classList.remove('open');
+//         fullImg.classList.remove('open');
+//     }
+// })

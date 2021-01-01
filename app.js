d1f396ef9c3e469b0d77f2b1d51cc6592ed086db
Ajose Tosin <tosinemidastouch@gmail.com>
2021-01-01 20:40:20 +0100

    pushing into the image gallery repo

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const fullImg = document.querySelector('.full-img');
+const smallImg = document.querySelectorAll('.gallery img');
+const modal = document.querySelector('.modal');
+
+// console.log(smallImg);
+
+smallImg.forEach(img => {
+    img.addEventListener('click', function () {
+        modal.classList.add('open');
+        fullImg.classList.add('open');
+
+        //changing the images dynamically
+        const originalQuality = img.getAttribute('alt');
+        fullImg.src = `img/full/${originalQuality}.jpg`
+    });
+});
+
+modal.addEventListener('click', function (e) {
+    if (e.target.classList.contains("modal")) {
+        modal.classList.remove('open');
+        fullImg.classList.remove('open');
+    }
+})
